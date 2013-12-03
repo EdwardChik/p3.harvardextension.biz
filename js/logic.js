f6d8dd29993a93cfcb63444c196970b2d8c5f9de
Edward Chik <edward.chik@gmail.com>
2013-12-03 12:39:16 -0500

    Added audio tracks, more basic template code

---
+++ js/logic.js
@@ -0,0 +1,19 @@
+// logic to pull RSS feed content into display page
+function randomSelect() {
+
+	// create an array of songs + characters
+	var catalog = [];
+
+	catalog['mario'] = "super_mario_world.mid";
+	catalog['link'] = "legend_of_zelda_.mid";
+	catalog['samus'] = "super_metroid.mid";
+	catalog['donkey_kong'] = "donkey_kong_country.mid";
+	catalog['star_fox'] = "star_fox.mid";
+
+	// randomly select song + character (integer)
+	var option = math.floor(math.random()*6);
+
+
+
+
+}
